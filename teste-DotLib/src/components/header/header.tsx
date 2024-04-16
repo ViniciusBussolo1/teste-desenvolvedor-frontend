2e1b66821ed1475b1dd59c8cfa904a60a86825fd
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-04-16 11:19:08 -0300

    :sparkles: feat: criado o hook useTheme para alterar entre dark e light

--- teste-DotLib/src/components/header/header.tsx
+++ teste-DotLib/src/components/header/header.tsx
@@ -1,17 +1,34 @@
 import * as Switch from '@radix-ui/react-switch'
 
+import { useTheme } from '../../hooks/useTheme'
+
+import { Sun, Moon } from 'lucide-react'
 import './header.scss'
 
 export function Header() {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <header className="header">
+    <header className={`header ${theme}`}>
       <h1>
         <span className="span">Dot</span>.Lib
       </h1>
 
-      <Switch.Root className="SwitchRoot" id="airplane-mode">
-        <Switch.Thumb className="SwitchThumb" />
-      </Switch.Root>
+      <div className="theme">
+        <Switch.Root
+          className="SwitchRoot"
+          id="airplane-mode"
+          checked={theme === 'dark'}
+          onCheckedChange={(event) => setTheme(event ? 'dark' : 'light')}
+        >
+          <Switch.Thumb className="SwitchThumb" />
+        </Switch.Root>
+        {theme === 'light' ? (
+          <Sun size={20} className="icon-light" />
+        ) : (
+          <Moon size={20} className="icon-dark" />
+        )}
+      </div>
     </header>
   )
 }
