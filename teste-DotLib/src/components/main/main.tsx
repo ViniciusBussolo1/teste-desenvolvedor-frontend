df3d17991a779920f7d335ca20881f9014f1b50a
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-04-16 15:50:20 -0300

    :sparkles: feat: implementa o hook de busca por nome do medicamento ou laboratório

--- teste-DotLib/src/components/main/main.tsx
+++ teste-DotLib/src/components/main/main.tsx
@@ -2,12 +2,16 @@ import { Search } from 'lucide-react'
 
 import './main.scss'
 import { Table } from '../table/table'
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import axios from 'axios'
 import { bulasDataProps } from '../../type/bulas'
+import { useBulaFilter } from '../../hooks/useBulaFilter'
 
 export function Main() {
   const [bulas, setBulas] = useState({} as bulasDataProps)
+  const [searchBula, setSearchBula] = useState('')
+
+  const { handleSearchBulas } = useBulaFilter()
 
   const baseURL = 'http://localhost:3000'
 
@@ -17,17 +21,24 @@ export function Main() {
     })
   }, [])
 
+  const handleSubmitSearchBulas = (event: FormEvent) => {
+    event.preventDefault()
+    handleSearchBulas(searchBula)
+  }
+
   return (
     <main className="main">
       <div className="container-main">
         <h1>Bulário Eletrônico</h1>
 
-        <form className="form">
+        <form onSubmit={handleSubmitSearchBulas} className="form">
           <input
             type="text"
             placeholder="Pesquise pelo medicamento ou laboratório..."
+            value={searchBula}
+            onChange={(e) => setSearchBula(e.target.value)}
           />
-          <button>
+          <button type="submit">
             <Search className="search" />
           </button>
         </form>
---
+++ teste-DotLib/src/hooks/useBulaFilter.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { bulasDataProps } from '../type/bulas'
+import axios from 'axios'
+
+export interface bulasData {
+  id: string
+  name: string
+  published_at: string
+  company: string
+  documents: {
+    id: string
+    expedient: string
+    type: string
+    url: string
+  }
+  active_principles: {
+    id: string
+    name: string
+  }
+}
+
+export function useBulaFilter() {
+  const [bulasFilter, setbulasFilter] = useState<Array<bulasDataProps>>([])
+
+  function removerSpecials(texto: string) {
+    // eliminando acentuação
+    texto = texto.replace(/[ÀÁÂÃÄÅ]/, 'A')
+    texto = texto.replace(/[àáâãäå]/, 'a')
+    texto = texto.replace(/[ÈÉÊË]/, 'E')
+    texto = texto.replace(/[Ç]/, 'C')
+    texto = texto.replace(/[ç]/, 'c')
+
+    return texto.replace(/[^a-z0-9]/gi, '')
+  }
+
+  async function handleSearchBulas(textFilter: string) {
+    const baseURL = 'http://localhost:3000'
+
+    const response = await axios.get(`${baseURL}/data`).then((response) => {
+      return response.data
+    })
+
+    const bulasFiltered = response.filter((item: bulasData) => {
+      return (
+        removerSpecials(item.name)
+          .toLocaleLowerCase()
+          .includes(removerSpecials(textFilter).toLocaleLowerCase()) ||
+        removerSpecials(item.company)
+          .toLocaleLowerCase()
+          .includes(removerSpecials(textFilter).toLocaleLowerCase())
+      )
+    })
+
+    setbulasFilter(bulasFiltered)
+  }
+
+  return { bulasFilter, handleSearchBulas }
+}
