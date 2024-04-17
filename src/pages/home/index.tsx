af60cc4733430ddfc7e9b4d2eff51774aaef4aee
Paulo <paulorfsantos17@gmail.com>
2024-04-17 19:52:59 -0300

    feat: add FormSearch in Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,20 @@
+import { Button } from "../../Components/Button";
+import { Input } from "../../Components/Input";
 import { Table } from "../../Components/Table";
 import { HomeContainer } from "./styles";
+import {MagnifyingGlass } from 'phosphor-react'
 
 export default function Home() {
   return (
       <HomeContainer>
           <h1>Listagem de Remedios</h1>
           <form>
-            <input type="text" placeholder="Pesquisar" />
-            <button type="submit">Pesquisar</button>
+            <Input type="text" placeholder="Digite o nome do remedio..." />
+            <Input type="text" placeholder="Digite o nome do remedio..." />
+            <Button type="submit" icon={<MagnifyingGlass  size={16} weight="bold"/>}>
+  
+              Pesquisar
+              </Button>
           </form>
           <Table />
         </HomeContainer>
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -3,4 +3,10 @@ import styled from "styled-components";
 export const HomeContainer = styled.main`
   padding: 2rem 5rem;
   
+
+  form {
+    margin-top: 2rem;
+    display: flex;
+    gap: 2rem;
+  }
 `;
