53f3c698cb0d4650be2f6ed10fbd7c2de624048a
Paulo <paulorfsantos17@gmail.com>
2024-04-17 16:55:15 -0300

    feat: add Header Component

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from "./styles";
+
+export  function Header() {
+  return (
+    <HeaderContainer>
+      <div>Logo</div>
+    </HeaderContainer>
+  )
+}
---
+++ src/Components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  left: 0;
+  width: 100%;
+  padding: 1.5rem 5rem;
+  border-bottom: .5px solid ${props => props.theme['gray-300']};
+`;
