dd5de34b5fcc57c1d767305b8872b85149b816a6
Paulo <paulorfsantos17@gmail.com>
2024-04-17 16:55:37 -0300

    styles: change color use color css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     background-color: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme['gray-200']};
     --webkit-font-smoothing: antialiased;
   }
 
