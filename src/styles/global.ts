0835c35b06721d754e8c17530dab04d3f6c1a16d
Paulo <paulorfsantos17@gmail.com>
2024-04-20 14:54:16 -0300

    style: add style scroll bar in global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,9 +19,37 @@ export const GlobalStyle = createGlobalStyle`
 
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
+
     font-size: 1rem;
     font-weight: 400;
   }
 
   a {text-decoration: none}
+
+  ::-webkit-scrollbar {
+  width: 10px;
+  border: none
+  
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  margin: .5rem .5rem;
+  
+  background: ${(props) => props.theme['gray-700']};
+  border-radius: 10px;
+
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: ${(props) => props.theme['gray-400']};
+  border-radius: 10px;
+
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: ${(props) => props.theme['gray-500']};
+}
 `
