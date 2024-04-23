943a768b9c9644bbb26a06a7be8d311a57d89ab5
Paulo <paulorfsantos17@gmail.com>
2024-04-23 14:22:11 -0300

    feat: ensure listing PrincipleActve

--- src/Components/ListPrincipleActives/index.tsx
+++ src/Components/ListPrincipleActives/index.tsx
@@ -1,13 +1,24 @@
+import { PrincipleActive } from '../../pages/NewRemedy'
 import { CelContent, CelHeader, Line, Table } from '../Table/style'
 
-export default function ListPrincipleActives() {
+interface ListPrincipleActivesProps {
+  principleActives: PrincipleActive[]
+}
+
+export default function ListPrincipleActives({
+  principleActives,
+}: ListPrincipleActivesProps) {
   return (
     <div>
       <Table>
-        <Line>
-          <CelHeader>Principio ativo:</CelHeader>
-          <CelContent>name</CelContent>
-        </Line>
+        {principleActives.map((principleActive) => {
+          return (
+            <Line key={principleActive.id}>
+              <CelHeader>Principio ativo:</CelHeader>
+              <CelContent>{principleActive.activePrinciple}</CelContent>
+            </Line>
+          )
+        })}
       </Table>
     </div>
   )
