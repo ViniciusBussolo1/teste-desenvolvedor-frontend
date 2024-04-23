2dc08cf150e93defa7f6daf2567b55d77fdbb7e7
Paulo <paulorfsantos17@gmail.com>
2024-04-23 10:36:19 -0300

    feat: add label in Input

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -4,12 +4,15 @@ import { ContainerInput, ErrorMessage, InputStyle } from './styles'
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string
+  label?: string
+  htmlFor?: string
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ error, ...props }, ref) => {
+  ({ error, label, htmlFor, ...props }, ref) => {
     return (
       <ContainerInput>
+        {label ? <label htmlFor={htmlFor}>{label}</label> : null}
         <InputStyle {...props} ref={ref} />
         {error && <ErrorMessage>{error}</ErrorMessage>}
       </ContainerInput>
