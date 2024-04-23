874fb32f2057fe73cc569bc5f678dfab2111ccd0
Paulo <paulorfsantos17@gmail.com>
2024-04-23 10:07:59 -0300

    feat: add routes new and update

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/home'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { UpdateRemedy } from './pages/UpdateRemedy'
+import { NewRemedy } from './pages/NewRemedy'
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/remedy/new',
+        element: <NewRemedy />,
+      },
+      {
+        path: '/remedy/:id/update',
+        element: <UpdateRemedy />,
       },
     ],
   },
