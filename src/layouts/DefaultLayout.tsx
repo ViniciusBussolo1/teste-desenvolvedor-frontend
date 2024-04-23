6f883e586413a4e2227dacd5969101ebb2bc6077
Paulo <paulorfsantos17@gmail.com>
2024-04-23 16:07:47 -0300

    refactor: Provider Toaster of  App for DefaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,19 @@
+import { Toaster } from 'sonner'
 import { Header } from '../Components/Header'
 import { Outlet } from 'react-router-dom'
 
 export function DefaultLayout() {
   return (
     <>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: '#FFF',
+            padding: '1rem',
+          },
+        }}
+        richColors={true}
+      />
       <Header />
       <Outlet />
     </>
