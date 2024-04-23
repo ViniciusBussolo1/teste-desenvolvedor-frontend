8c824cb279c6f654865ef6626bd450eb4ddab885
Paulo <paulorfsantos17@gmail.com>
2024-04-23 10:35:52 -0300

    feat: add Links and Nav Bar in Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,9 +1,17 @@
-import { HeaderContainer } from './styles'
+import { HeaderContainer, NavContainer, LinkStyle } from './styles'
+import { Button } from '../Button'
 
 export function Header() {
   return (
     <HeaderContainer>
       <div>Logo</div>
+
+      <NavContainer>
+        <LinkStyle to="/">Home</LinkStyle>
+        <LinkStyle to="/remedy/new">
+          <Button variant="success">Adicionar Remedio</Button>
+        </LinkStyle>
+      </NavContainer>
     </HeaderContainer>
   )
 }
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
@@ -5,4 +6,21 @@ export const HeaderContainer = styled.header`
   width: 100%;
   padding: 1.5rem 5rem;
   border-bottom: 0.5px solid ${(props) => props.theme['gray-300']};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const LinkStyle = styled(Link)`
+  color: ${(props) => props.theme['gray-200']};
+  transition: 0.3s ease-out;
+
+  &:hover {
+    color: ${(props) => props.theme['gray-100']};
+  }
 `
