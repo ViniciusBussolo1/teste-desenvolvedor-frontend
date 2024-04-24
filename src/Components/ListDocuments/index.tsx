a9f64ea70ca84ab6c389dce314ac926d584cda34
Paulo <paulorfsantos17@gmail.com>
2024-04-24 09:43:45 -0300

    refactor: ensure view EmpytMessage when list of document if empyt

--- src/Components/ListDocuments/index.tsx
+++ src/Components/ListDocuments/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { ListContainer } from './style'
 import { CelHeader, Line, Table } from '../Table/style'
 import { DocumentRemedy } from '../../pages/NewRemedy'
+import EmpytMessage from '../EmpytMessage'
 
 interface ListDocumentsProps {
   documents: DocumentRemedy[]
 }
 
 export default function ListDocuments({ documents }: ListDocumentsProps) {
+  if (documents.length === 0) {
+    return <EmpytMessage message="Não há documentos adicionados." />
+  }
   return (
     <ListContainer>
       <Table>
