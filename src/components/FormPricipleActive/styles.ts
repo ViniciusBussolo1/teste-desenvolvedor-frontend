c1a59d8eff90f7297314c7b9662813de7039dbbd
Paulo <paulorfsantos17@gmail.com>
2024-04-25 09:51:16 -0300

    feat: add responsive in formPrincipleActive

--- src/components/FormPricipleActive/styles.ts
+++ src/components/FormPricipleActive/styles.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
+import { devices } from '../../styles/theme/devices'
 
 export const FormPrincipleAtiveStyle = styled.form`
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: start;
   gap: 1.5rem;
   button {
     width: 30%;
+  }
+  @media ${devices.mobile} {
+    button {
+      width: 70%;
+    }
   }
 `
