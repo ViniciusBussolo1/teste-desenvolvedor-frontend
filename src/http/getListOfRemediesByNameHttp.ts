c59711d7beac15371fec99135b745b5fa1e26af0
Paulo <paulorfsantos17@gmail.com>
2024-04-25 16:04:46 -0300

    refactor:  rename function and add interface

---
+++ src/http/getListOfRemediesByNameHttp.ts
@@ -0,0 +1,57 @@
+import { api } from '../lib/axios'
+
+interface Remedy {
+  id: string
+  name: string
+  published_at: string
+  company: string
+  documents: {
+    id: string
+    expedient: string
+    type: 'PROFESSIONAL' | 'PATIENT'
+    url: string
+  }[]
+  active_principles?: {
+    id: string
+    name: string
+  }[]
+}
+
+interface RemedyResponse {
+  remediesData: Remedy[]
+  totalItems: number
+}
+
+export async function getListOfRemediesByNameHttp(
+  name: string,
+  page: number,
+): Promise<RemedyResponse> {
+  try {
+    const remediesResponse = await api.get('/data', {
+      params: {
+        name_like: name.toUpperCase(),
+        _sort: 'published_at',
+        _order: 'desc',
+        _page: page,
+      },
+    })
+    const totalItems = remediesResponse.headers['x-total-count']
+
+    const remediesData: Remedy[] = remediesResponse.data
+    const RemedyResponseStatus = remediesData.map((remedyData) => {
+      return {
+        ...remedyData,
+        principleActives: remedyData.active_principles
+          ? remedyData.active_principles
+          : [],
+      }
+    })
+
+    return {
+      remediesData: RemedyResponseStatus,
+      totalItems,
+    }
+  } catch (error) {
+    throw new Error('Erro ao fazer a busca.')
+  }
+}
